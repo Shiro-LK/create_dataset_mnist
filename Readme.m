758581659662a46d9c82ade6fd8780da232da2be
Shiro-LK <shirosaki94@gmail.com>
2017-08-14 14:17:48 +0200

    update readme

--- Readme.m
+++ Readme.m
@@ -1,3 +1,5 @@
+# create mnist dataset 
+
 Script using Keras in order to save mnist dataset in .jpg format.
 
 For more information about this dataset, look at this website : http://yann.lecun.com/exdb/mnist/
